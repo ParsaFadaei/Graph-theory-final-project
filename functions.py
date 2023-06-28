b4d3e7b319ec01dee04f5364db0e6f0ac698e0ec
Parsa <parsafadaei@yahoo.com>
2023-06-28 15:40:51 +0330

    second commit

---
+++ functions.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+
+def show3dSpring(G):
+    pos = nx.spring_layout(G, dim=3, seed=779)
+    # Extract node and edge positions from the layout
+    node_xyz = np.array([pos[v] for v in sorted(G)])
+    edge_xyz = np.array([(pos[u], pos[v]) for u, v in G.edges()])
+
+    # Create the 3D figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Plot the nodes - alpha is scaled by "depth" automatically
+    ax.scatter(*node_xyz.T, s=100, ec="w")
+
+    # Plot the edges
+    for vizedge in edge_xyz:
+        ax.plot(*vizedge.T, color="tab:gray")
+
+    _format_axes(ax)
+    fig.tight_layout()
+    plt.show()
+
+
+def _format_axes(ax):
+    """Visualization options for the 3D axes."""
+    # Turn gridlines off
+    ax.grid(True)
+    # Suppress tick labels
+    for dim in (ax.xaxis, ax.yaxis, ax.zaxis):
+        dim.set_ticks([])
+    # Set axes labels
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import functions
+
+tetra = nx.tetrahedral_graph()
+# functions.show3dSpring(tetra)
 
