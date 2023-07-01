f0b1266dcfe902f3e4088d1302f6d74ec6c8a985
Parsa <parsafadaei@yahoo.com>
2023-07-02 01:43:34 +0330

    final

--- main.py
+++ main.py
@@ -10,48 +10,48 @@ import functions
 import LayeredGraph
 
 """"PART 3 ,5, 6"""
-# erdos = nx.erdos_renyi_graph(30, p=0.3)
+erdos = nx.erdos_renyi_graph(100, p=0.3)
 # tetra = nx.tetrahedral_graph()
 # octa = nx.octahedral_graph()
 # cube = nx.cubical_graph()
 # ico = nx.icosahedral_graph()
 # truncTetra = nx.truncated_tetrahedron_graph()
 # hyperCube = nx.hypercube_graph(4)
-completeN = nx.complete_graph(5)
-Torus = nx.complete_graph(7)
-ring = functions.ring_graph(10, 1)
-prism3 = functions.schlegel3Prism()
-Antiprism3 = functions.schlegel3Antiprism()
-Twistedprism3 = functions.sclegel3TwistedPrism()
+# completeN = nx.complete_graph(5)
+# Torus = nx.complete_graph(7)
+# ring = functions.ring_graph(10, 1)
+# prism3 = functions.schlegel3Prism()
+# Antiprism3 = functions.schlegel3Antiprism()
+# Twistedprism3 = functions.sclegel3TwistedPrism()
 
 hamming = functions.hamming_binary(4)
 
 # Wats = nx.watts_strogatz_graph(1000, 3, 0.3)
 
 
-G = hamming
+G = erdos
 # H = prism3
 functions.setAllCaps(G)
 
 
-# functions.getInfo(G)
+functions.getInfo(G)
 
 """"CALCULATING L"""
 L = float(0)
 newL = float(0)
-L = functions.calculateLHamming(G)
+# L = functions.calculateLHamming(G)
 # L = functions.calculateLforSymm(G)
 # L = functions.calculateL(G)
 # L1 = functions.calculateLmax(G)
 #
-functions.remove_rnd_node(G, 0.2)
-newL = functions.calculateLHamming(G)
+# functions.remove_rnd_node(G, 0.2)
+# newL = functions.calculateLHamming(G)
 # newL = functions.calculateLforSymm(G)
 # newL = functions.calculateL(G)
 # newL1 = functions.calculateLmax(G)
-increasePerc = functions.changePerc(newL, L)
+# increasePerc = functions.changePerc(newL, L)
 #
-print("INCREASE Perc =", increasePerc, "%")
+# print("INCREASE Perc =", increasePerc, "%")
 """PART 7 & 8"""
 
 # T = functions.createLayeredGraph(G, H);
